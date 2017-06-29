52c6d589d3614803c66a6cc1740d81cefdd3362e
dyadyaJora <ja_massa97@mail.ru>
2017-06-29 20:19:15 +0300

    подключил bem-history

--- .enb/make.js
+++ .enb/make.js
@@ -27,6 +27,7 @@ var techs = {
         { path: 'node_modules/bem-components/desktop.blocks', check: false },
         { path: 'node_modules/bem-components/design/common.blocks', check: false },
         { path: 'node_modules/bem-components/design/desktop.blocks', check: false },
+        { path: 'node_modules/bem-history/common.blocks', check: false },
         'common.blocks'
     ];
 
