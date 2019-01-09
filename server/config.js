7c14448ea5d6abd8c73552d280fb7a15c5e9de9f
Vladislav Telichko <vladislav.telichko@yandex.ru>
2019-01-09 18:04:51 +0300

    Change port

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     staticFolder: 'static',
-    defaultPort: 3000,
+    defaultPort: 3010,
     cacheTTL: 30000,
     sessionSecret: 'REPLACE_ME_WITH_RANDOM_STRING',
     database: {
