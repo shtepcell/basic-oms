9700ba54a8d460cafefb23070c18588db29758a7
Vladislav Telichko <vladislav.telichko@ya.ru>
2019-01-14 01:25:17 +0300

    Some change

--- tools/filesPortable.js
+++ tools/filesPortable.js
@@ -30,12 +30,8 @@ function checkPath(path) {
 }
 
 (async() => {
-    const query = {
-        $or: [
-            { "info.file-init": {$ne: null} },
-            { "info.order": {$ne: null} }
-        ]
-    }
+    const query = { "info.order": {$ne: null} };
+
     const orders = await Order.find(query);
 
     for (let index = 0; index < orders.length; index++) {
@@ -45,40 +41,12 @@ function checkPath(path) {
             if(order.isOld && checkPath(order.info.order)) {
                 const path = order.info.order;
                 const fileName = getFileName(path);
-                const newPath = `static/storage/${fileName}`;
-                fs.rename('static/files' + path, newPath, (err) => {
-                    if (err) {
-                        console.log(`Error ${path} ${order.id}`);
-                        order.info.order = null;
-                        order.save();
-                        return;
-                    }
-                    order.info.order = fileName;
-                    order.save();
-                })
-            } else {
-                const fileName = order.info.order;
-                const path = generatePath(order.id) + fileName;
 
-                fs.rename(path, `static/storage/${fileName}`, (err) => {
-                    if (err) {
-                        console.log(`Error ${path} ${order.id}`);
-                        return;
-                    }
-                })
-            }
-        }
-
-        if (order.info['file-init']) {
-            const fileName = order.info['file-init'];
-            const path = generatePath(order.id) + fileName;
-
-            fs.rename(path, `static/storage/${fileName}`, (err) => {
-                if (err) {
-                    console.log(`Error ${path} ${order.id}`);
-                    return;
-                }
-            })
+                console.log('Saved #'+ order.id);
+                
+                order.info.order = fileName;
+                order.save();
+            } 
         }
     }
 })();
