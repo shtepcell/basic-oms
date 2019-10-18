0bcb789eae7a0e4316b49a357eef9e43431414ab
Vladislav Telichko <vladislav.telichko@ya.ru>
2019-10-18 17:14:40 +0300

    Добавил метрику

--- .enb/make.js
+++ .enb/make.js
@@ -29,6 +29,7 @@ var techs = {
         { path: 'node_modules/bem-components/design/desktop.blocks', check: false },
         { path: 'node_modules/bem-history/common.blocks', check: false },
         { path: 'node_modules/bem-calendar/common.blocks', check: false },
+        { path: 'node_modules/bem-stat-counters/common.blocks', check: false },
         'common.blocks'
     ];
 
--- common.blocks/page/page.bemtree.js
+++ common.blocks/page/page.bemtree.js
@@ -1,6 +1,17 @@
 block('page').content()(function() {
     return [
         {
+            block: 'yandex-metrica',
+            params: {
+                id: 55831267,
+                webvisor: true,
+                clickmap: true,
+                trackLinks: true,
+                accurateTrackBounce: true,
+                v2: true
+            }
+        },
+        {
             block: 'header'
         },
         {
