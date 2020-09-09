cfb703ed2f48bf392d7d3f60e5ff1b9f0697220c
Vladislav Telichko <vladislav.telichko@ya.ru>
2020-09-09 17:27:07 +0300

    Выходить из скрипта внутри try

--- scripts/calculateStat.js
+++ scripts/calculateStat.js
@@ -15,11 +15,9 @@ process.on('unhandledRejection', errorHandler);
 (async () => {
     try {
         const stat = await getStat();
-
         fs.writeFileSync(path.resolve('cache/status.json'), JSON.stringify(stat));
+        process.exit(0);
     } catch (err) {
         await errorHandler(err);
     }
-
-    process.exit(0);
 })();
