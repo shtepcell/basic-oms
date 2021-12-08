a22a66837ae9ebbe6c8d1927d130f1f1ae2428ec
shtepcell <vladislav.telichko@ya.ru>
2021-12-08 20:52:52 +0300

    Update import script

--- scripts/importOrder.js
+++ scripts/importOrder.js
@@ -26,72 +26,76 @@ const getCityType = (type) => {
     }[type.toLowerCase()];
 }
 
+const getService = (service) => ({
+    'l2vpn': 'l2vpn' 
+}[service.toLowerCase()]);
+
 (async () => {
-    const podolo = await Account.findOne({ login: 'podolko' });
+    const initiator = await Account.findOne({ login: 'erokhin' });
     const department = await Department.findOne({ type: 'b2o' });
-    const client = await Client.findOne({ name: 'Министерство цифрового развития, связи и массовых коммуникаций Российской Федерации (Проект СЗО)' });
+    const client = await Client.findOne({ name: 'Министерство цифрового развития, связи и массовых коммуникаций Российской Федерации (Проект ЕСПД)' });
 
     const ids = [];
-
+    
     for (const item of data) {
         const nowDate = new Date();
 
         let city = await City.findOne({ name: trim(item, 'Наименование н.п') });
-        const provider = await Provider.findOne({ name: item['Оператор ПМ из СУЗ'].slice(7) })
+        // const provider = await Provider.findOne({ name: item['Оператор ПМ из СУЗ'].slice(7) })
 
         if (!city) {
             console.log(`Cannot find city ${trim(item, 'Наименование н.п')}`);
 
             city = new City({
-                type: getCityType(item['Тип населённого пункта']),
+                type: getCityType(item['Тип населённого пункта'] || item['Тип населенного пункта']),
                 name: trim(item, 'Наименование н.п'),
                 usage: true
             });
 
             city.save();
-            console.log(`Created city ${getCityType(item['Тип населённого пункта'])} ${trim(item, 'Наименование н.п')}`)
+            console.log(`Created city ${getCityType(item['Тип населённого пункта'] || item['Тип населенного пункта'])} ${trim(item, 'Наименование н.п')}`)
         }
 
-        if (!provider) {
-            console.log(`Cannot find provider ${item['Оператор ПМ из СУЗ'].slice(6)}`);
-            continue;
-        }
+        // if (!provider) {
+        //     console.log(`Cannot find provider ${item['Оператор ПМ из СУЗ'].slice(6)}`);
+        //     continue;
+        // }
 
         const order = new Order({
             id: await Static.getOrderId(),
-            status: 'stop-build',
-            deadline: await helper.calculateDeadline(30),
+            status: 'gzp-build',
+            deadline: await helper.calculateDeadline(Number(item[' Контрольный срок '])),
             info: {
-                initiator: podolo,
+                initiator: initiator,
                 department,
-                idoss: '*',
+                idoss: item[' ID OSS* '] || '*',
                 add_info: item['Дополнительная информация'].trim(),
                 contact: item['Контактные данные клиента'].trim(),
-                service: 'internet',
+                service: getService(item['Услуга']),
                 clientType : "Крупный бизнес-клиент",
                 'income-once': item[' Ожидаемый единовр. доход (руб) '].trim(),
                 'income-monthly': item[' Ожидаемый ежемес. доход (руб) '].trim(),
                 city: city,
-                volume : '10Мб/c',
+                volume : item['Емкость Мб/с'],
 		        ip : '4 (факт. 1) = /30',
                 coordinate: item['Полный адрес по координатам'].trim(),
-                client
+                client,
+                cms: item['CMS'],
             },
-            stop: {
+            gzp: {
                 'cost-monthly': item[' Операционные затраты ежемесячные '].trim(),
                 'cost-once': item[' Единоразовая стоимость организации '].trim(),
-                time: 30,
-                contact: '_',
+                time: Number(item[' Контрольный срок ']),
                 capability: true,
                 complete: true,
-                provider: provider,
+                need: true,
             },
             date: { 
                 "init" : nowDate,
-                "cs-stop-pre" : nowDate,
-                "stop-pre" : nowDate,
+                "cs-gzp-pre" : nowDate,
+                "gzp-pre" : nowDate,
                 "client-match" : nowDate,
-                "cs-stop-organization" : nowDate
+                "cs-gzp-organization" : nowDate
             },
             history: [
                 {
@@ -102,7 +106,7 @@ const getCityType = (type) => {
                 {
                     "author" : "Робот СУЗ",
                     "date" : nowDate,
-                    "name" : "Проработка СТОП/VSAT",
+                    "name" : "Проработка ГЗП",
                 },
                 {
                     "author" : "Робот СУЗ",
