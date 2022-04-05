e81fe8793d0ffe0df4f79fe747738d9f1be2912b
shtepcell <vladislav.telichko@ya.ru>
2022-04-05 18:10:03 +0300

    add dotenv

--- server/index.js
+++ server/index.js
@@ -1,144 +1,155 @@
-Object.assign || (Object.assign = require('object-assign'));
+Object.assign || (Object.assign = require("object-assign"));
+require("dotenv").config();
 
-const moment = require('moment');
-const next = require('next')
-const Auth = require('./controllers/auth');
-const Mass = require('./controllers/mass');
+const moment = require("moment");
+const next = require("next");
+const Auth = require("./controllers/auth");
+const Mass = require("./controllers/mass");
 
-var fs = require('fs'),
-    path = require('path'),
-    express = require('express'),
+var fs = require("fs"),
+    path = require("path"),
+    express = require("express"),
     app = express(),
-    bodyParser = require('body-parser'),
-    favicon = require('serve-favicon'),
-    morgan = require('morgan'),
-    serveStatic = require('serve-static'),
-    cookieParser = require('cookie-parser'),
-    expressSession = require('express-session'),
-    passport = require('passport'),
-    compression = require('compression'),
-    logger = require('./controllers/logger'),
-    config = require('./config'),
+    bodyParser = require("body-parser"),
+    favicon = require("serve-favicon"),
+    morgan = require("morgan"),
+    serveStatic = require("serve-static"),
+    cookieParser = require("cookie-parser"),
+    expressSession = require("express-session"),
+    passport = require("passport"),
+    compression = require("compression"),
+    logger = require("./controllers/logger"),
+    config = require("./config"),
     staticFolder = config.staticFolder,
-
-    Render = require('./render'),
+    Render = require("./render"),
     render = Render.render,
-
     port = process.env.PORT || config.defaultPort,
     isSocket = isNaN(port),
-    isDev = process.env.NODE_ENV !== 'production',
+    isDev = process.env.NODE_ENV !== "production",
+    mongoose = require("./controllers/connect"),
+    MongoStore = require("connect-mongo")(expressSession),
+    helper = require("./controllers/helper"),
+    router = require("./router");
 
-    mongoose = require('./controllers/connect'),
-    MongoStore = require('connect-mongo')(expressSession),
-
-    helper = require('./controllers/helper'),
-    router = require('./router');
-
-require('debug-http')();
+require("debug-http")();
 
 const nextApp = next({ dev: isDev });
 const handle = nextApp.getRequestHandler();
 
-nextApp.prepare()
-    .catch((ex) => {
-        console.error(ex.stack)
-        process.exit(1)
-    });
-    
-morgan.token('date', function() {
+nextApp.prepare().catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+});
+
+morgan.token("date", function () {
     var p = helper.dateToExtStr(new Date());
     return p;
 });
 
-morgan.token('user', function(req, res) {
-    if(res.locals.__user) {
+morgan.token("user", function (req, res) {
+    if (res.locals.__user) {
         return res.locals.__user.login;
     } else {
-        return 'guest'
+        return "guest";
     }
 });
 
-morgan.token('smart-url', function(req, res) {
-    if(req.originalUrl.length > 90) return req.originalUrl.substring(0, 70) + '.-.-.-.' + req.originalUrl.substring(req.originalUrl.length-20, req.originalUrl.length);
+morgan.token("smart-url", function (req, res) {
+    if (req.originalUrl.length > 90)
+        {return (
+            req.originalUrl.substring(0, 70) +
+            ".-.-.-." +
+            req.originalUrl.substring(
+                req.originalUrl.length - 20,
+                req.originalUrl.length
+            )
+        );}
     return req.originalUrl;
 });
 
-app
-    .disable('x-powered-by')
-    .enable('trust proxy')
+app.disable("x-powered-by")
+    .enable("trust proxy")
     .use(compression())
-    .use(favicon(path.join(staticFolder, 'favicon.ico')))
+    .use(favicon(path.join(staticFolder, "favicon.ico")))
     .use(serveStatic(staticFolder))
-    .use(morgan('[HTTP] :date[web] <:user> :method :smart-url :status :res[header] - :response-time ms'))
+    .use(
+        morgan(
+            "[HTTP] :date[web] <:user> :method :smart-url :status :res[header] - :response-time ms"
+        )
+    )
     .use(cookieParser())
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended: true }))
-    .use(expressSession({
-        name: 'basic-oms',
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            maxAge: 14 * 24 * 60 * 60 * 1000 // 2 недели
-        },
-        unset: 'destroy',
-        secret: 's562hdnYsd52nSBN',
-        // secret: secretConf.secret,
-        store: new MongoStore({
-            mongooseConnection: mongoose.connection,
-            autoRemove: 'native',
-            ttl: 14 * 24 * 60 * 60,
-            touchAfter: 10 * 60,
-            stringify: true
+    .use(
+        expressSession({
+            name: "basic-oms",
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                maxAge: 14 * 24 * 60 * 60 * 1000, // 2 недели
+            },
+            unset: "destroy",
+            secret: "s562hdnYsd52nSBN",
+            // secret: secretConf.secret,
+            store: new MongoStore({
+                mongooseConnection: mongoose.connection,
+                autoRemove: "native",
+                ttl: 14 * 24 * 60 * 60,
+                touchAfter: 10 * 60,
+                stringify: true,
+            }),
         })
-    }))
+    )
     .use(passport.initialize())
     .use(passport.session());
 
 // NOTE: conflicts with livereload
 // isDev || app.use(slashes());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, JSON.stringify(user));
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser(function (user, done) {
     done(null, JSON.parse(user));
 });
 
-var server = require('http').createServer(app);
+var server = require("http").createServer(app);
 
-var io = require('socket.io')(server);
+var io = require("socket.io")(server);
 
-process.on('unhandledRejection', error => {
-    const time = moment().format('DD.MM.YYYY HH:mm:ss');
+process.on("unhandledRejection", (error) => {
+    const time = moment().format("DD.MM.YYYY HH:mm:ss");
 
     console.log(`${time} – unhandledRejection`, error);
 });
 
 router(app, io);
 
-app.get('/mass', Mass.getOwnRequestsMiddleware, handle);
-app.get('*', handle)
+app.get("/mass", Mass.getOwnRequestsMiddleware, handle);
+app.get("*", handle);
 
-isDev && require('./rebuild')(app);
+isDev && require("./rebuild")(app);
 
-app.get('*', function(req, res) {
+app.get("*", function (req, res) {
     res.status(404);
-    return render(req, res, { view: '404' });
+    return render(req, res, { view: "404" });
 });
 
 if (isDev) {
-    app.get('/error/', function() {
-        throw new Error('Uncaught exception from /error');
+    app.get("/error/", function () {
+        throw new Error("Uncaught exception from /error");
     });
 
-    app.use(require('errorhandler')());
+    app.use(require("errorhandler")());
 }
 
 isSocket && fs.existsSync(port) && fs.unlinkSync(port);
 
-server.listen(port, function() {
-    isSocket && fs.chmod(port, '0777');
-    console.log(`\n  ################## RELOAD SERVER - ${helper.dateToExtStr()} ################### \n`);
+server.listen(port, function () {
+    isSocket && fs.chmod(port, "0777");
+    console.log(
+        `\n  ################## RELOAD SERVER - ${helper.dateToExtStr()} ################### \n`
+    );
     logger.info(`Server is listening on ${this.address().port}`);
 });
