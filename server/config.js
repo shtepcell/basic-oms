e6ac79b0fb0015ab9d6e971ce613fd4d6b7e1c37
shtepcell <vladislav.telichko@ya.ru>
2022-09-13 11:19:19 +0400

    feat: enviroment variables

--- server/config.js
+++ server/config.js
@@ -1,11 +1,13 @@
+const { SECRET_KEY, SERVER_PORT, DATABASE_NAME } = process.env;
+
 module.exports = {
     staticFolder: 'static',
-    defaultPort: 3000,
+    defaultPort: SERVER_PORT || 3000,
     cacheTTL: 30000,
-    sessionSecret: 'REPLACE_ME_WITH_RANDOM_STRING',
+    sessionSecret: SECRET_KEY || 'REPLACE_ME_WITH_RANDOM_STRING',
     database: {
         port: 27017,
         url: "localhost",
-        name: "basic-oms"
+        name: DATABASE_NAME || "basic-oms"
     }
 };
